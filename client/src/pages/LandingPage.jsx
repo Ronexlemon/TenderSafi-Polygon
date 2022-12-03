650e5c9aa498aca07085b1a792abb15f1e265299
“dennohkim” <kimathidennis38@gmail.com>
2022-12-03 20:32:59 +0300

    feat: create header section

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import HeroText from '../components/HeroText'
+import Navbar from '../components/Navbar'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+      <header
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+        className="min-h-screen h-[100%] bg-no-repeat bg-cover bg-top bg-slate-100 flex flex-col"
+      >
+        <Navbar />
+        <HeroText />
+      </header>
+    </div>
   )
 }
 
