252bd380443f1f5be15b3be73691d9c3239f8233
RonexLemon <ronexondimu@gmail.com>
2022-12-03 11:40:15 +0300

    deployed the contract

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -9,6 +9,8 @@ async function main(){
     await TenderSafiPolygonContractdeploy.deployed();
     //consoloe the address
     console.log(TenderSafiPolygonContractdeploy.address);
+    //take this address
+    //TendersafiContractAddressis= "0x8fF171857abe05f4642e90Ec243A9553f0853678"
 }
 //call main
 main().then(()=>process.exit(0))
